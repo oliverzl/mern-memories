7297381f9d3ba8855e46b70473f64e9261bd0173
Oliver97 <oliverchong987@gmail.com>
2021-06-06 14:52:11 +0800

    Added like, delete, edit functionality.
    Editing the post will populate the text fields of the form, and change it to 'edit' instead of the default form creation.

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL, CREATE, DELETE, UPDATE } from "../constants/actionTypes";
 import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
 	try {
 		const { data } = await api.fetchPosts();
 
-		dispatch({ type: "FETCH_ALL", payload: data });
+		dispatch({ type: FETCH_ALL, payload: data });
 	} catch (error) {
 		console.log(error);
 	}
@@ -13,7 +14,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
 	try {
 		const { data } = await api.createPost(post);
-		dispatch({ type: "CREATE", payload: data });
+		dispatch({ type: CREATE, payload: data });
 	} catch (error) {
 		console.log(error);
 	}
@@ -23,7 +24,26 @@ export const updatePost = (id, post) => async (dispatch) => {
 	try {
 		const { data } = await api.updatePost(id, post);
 
-		dispatch({ type: "UPDATE", payload: data });
+		dispatch({ type: UPDATE, payload: data });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const deletePost = (id) => async (dispatch) => {
+	try {
+		await api.deletePost(id);
+		dispatch({ type: DELETE, payload: id });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const likePost = (id) => async (dispatch) => {
+	try {
+		const { data } = await api.likePost(id);
+
+		dispatch({ type: UPDATE, payload: data });
 	} catch (error) {
 		console.log(error);
 	}
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,9 +12,13 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
 
 	return (
 		<Card className={classes.card}>
@@ -49,17 +53,29 @@ const Post = ({ post, setCurrentId }) => {
 				{post.title}
 			</Typography>
 			<CardContent>
-				<Typography variant="h5" gutterBottom>
-					{post.message}
+				<Typography variant="body2" color="textSecondary" component="p">
+					Message: {post.message}
 				</Typography>
 			</CardContent>
 			<CardActions className={classes.cardActions}>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => {
+						dispatch(likePost(post._id));
+					}}
+				>
 					<ThumbUpAltIcon fontSize="small" />
-					Like
+					&nbsp; Like &nbsp;
 					{post.likeCount}
 				</Button>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => {
+						dispatch(deletePost(post._id));
+					}}
+				>
 					<DeleteIcon fontSize="small" />
 					Delete
 				</Button>
