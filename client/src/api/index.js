cb62a7a556a68d0793a59d10ffc6703e7aca29f2
Oliver97 <oliverchong987@gmail.com>
2021-06-08 00:11:35 +0800

    Added Auth, Input Component, SignIn and SignUp form
    this is the preparation of the frontend for Authentication, creating an account, signing in, and the form to create an account / sign in.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "http://localhost:5000/posts";
-const url = "https://mern-mem-v2.herokuapp.com/posts";
+const url = "http://localhost:5000/posts";
+// const url = "https://mern-mem-v2.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
