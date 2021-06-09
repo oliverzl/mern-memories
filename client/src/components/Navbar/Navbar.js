42a059c66ded78e3ed26f4ffe39bcf507b3a9187
Oliver97 <oliverchong987@gmail.com>
2021-06-09 18:33:01 +0800

    Added GoogleAuth sign in page.
    clicking sign in with google will cause the google sign in window to pop up.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Avatar, Typography, Toolbar, Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 const Navbar = () => {
 	const classes = useStyles();
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const location = useLocation();
 
-	const user = null;
+	const logout = () => {
+		dispatch({ type: "LOGOUT" });
+		history.push("/");
+		setUser(null);
+	};
+
+	useEffect(() => {
+		const token = user?.token;
+
+		setUser(JSON.parse(localStorage.getItem("profile")));
+	}, [location]);
+
 	return (
 		<AppBar className={classes.appBar} position="static" color="inherit">
 			<div className={classes.brandContainer}>
@@ -43,6 +59,7 @@ const Navbar = () => {
 							variant="contained"
 							className={classes.logout}
 							color="secondary"
+							onClick={logout}
 						>
 							Logout
 						</Button>
@@ -54,7 +71,6 @@ const Navbar = () => {
 						variant="contained"
 						color="primary"
 					>
-						{" "}
 						Sign In
 					</Button>
 				)}
