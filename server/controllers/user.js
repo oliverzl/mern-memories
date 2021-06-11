0ca2b2bbca934fab543d04aff5b79aeafec3a9ea
Oliver97 <oliverchong987@gmail.com>
2021-06-11 19:19:25 +0800

    User specific actions.
    added user specific functionalities, such as: Only the creator of the post can delete and edit the post, and the user must be signed in to delete/edit his/her own post, and like other's posts.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,7 +5,6 @@ import User from "../models/user.js";
 
 export const signin = async (req, res) => {
 	const { email, password } = req.body;
-
 	try {
 		const existingUser = await User.findOne({ email });
 		if (!existingUser) {
@@ -23,6 +22,7 @@ export const signin = async (req, res) => {
 			"test",
 			{ expiresIn: "1h" }
 		);
+
 		res.status(200).json({ result: existingUser, token });
 	} catch (error) {
 		res.status(500).json({ message: "Something went wrong" });
