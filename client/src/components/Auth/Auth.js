28e296b7d309a2f6ca7196548be517bb240b8a8b
Oliver97 <oliverchong987@gmail.com>
2021-06-11 08:29:54 +0800

    completed manual sign in & registration, as well as Auth middleware
    the manual sign in and registration creates new 'users' in our database, an addition to google OAUTH login. middleware implemented to user actions = user needs to be signed in to perform certain actions in the app.

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -15,24 +15,43 @@ import { useHistory } from "react-router-dom";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
 import Input from "./Input";
+import { signin, signup } from "../../actions/auth";
+
+const initialState = {
+	firstName: "",
+	lastName: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+};
 
 const Auth = () => {
 	const classes = useStyles();
 	const [showPassword, setShowPassword] = useState(false);
-	const [isSignup, setIsSignup] = useState(true);
+	const [isSignup, setIsSignup] = useState(false);
+	const [formData, setFormData] = useState(initialState);
 	const dispatch = useDispatch();
 	const history = useHistory();
 
 	const handleShowPassword = () =>
 		setShowPassword((prevShowPassword) => !prevShowPassword);
 
-	const handleSubmit = () => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (isSignup) {
+			dispatch(signup(formData, history));
+		} else {
+			dispatch(signin(formData, history));
+		}
+	};
 
-	const handleChange = () => {};
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
 
 	const switchMode = () => {
 		setIsSignup((prevIsSignup) => !prevIsSignup);
-		handleShowPassword(false);
+		setShowPassword(false);
 	};
 
 	const googleSuccess = async (res) => {
@@ -71,8 +90,8 @@ const Auth = () => {
 									half
 								/>
 								<Input
-									name="firstName"
-									label="First Name"
+									name="lastName"
+									label="Last Name"
 									handleChange={handleChange}
 									half
 								/>
