c0d1e99edd2ba0aae40c62bc195e259c3ad3219e
Oliver97 <oliverchong987@gmail.com>
2021-07-06 23:33:00 +0800

    fixed immediate reflection of a user liking a post instead of fetching posts which depends on API calls

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
@@ -15,21 +15,35 @@ const Post = ({ post, setCurrentId }) => {
 	const dispatch = useDispatch();
 	const user = JSON.parse(localStorage.getItem("profile"));
 	const history = useHistory();
+	const [likes, setLikes] = useState(post?.likes);
+
+	//getting either google id or user id from database, set it to userId
+	const userId = user?.result.googleId || user?.result._id;
+
+	const hasLikedPost = post.likes.find((like) => like === userId);
+
+	const handleLike = async () => {
+		dispatch(likePost(post._id));
+
+		if (hasLikedPost) {
+			setLikes(post.likes.filter((id) => id !== userId));
+		} else {
+			setLikes([...post.likes, userId]);
+		}
+	};
 
 	const Likes = () => {
-		if (post?.likes?.length > 0) {
-			return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) ? (
+		if (likes.length > 0) {
+			return likes.find((like) => like === userId) ? (
 				<>
 					<ThumbUpAltIcon fontSize="small" />
 					&nbsp;
-					{post.likes.length > 2
-						? `You and ${post.likes.length - 1} others`
-						: `${post.likes.length} like${post.likes.length > 1 ? "s" : ""}`}
+					{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? "s" : ""}`}
 				</>
 			) : (
 				<>
 					<ThumbUpAltOutlined fontSize="small" />
-					&nbsp;{post.likes.length} {post.likes.length === 1 ? "Like" : "Likes"}
+					&nbsp;{likes.length} {likes.length === 1 ? "Like" : "Likes"}
 				</>
 			);
 		}
@@ -82,14 +96,7 @@ const Post = ({ post, setCurrentId }) => {
 				</CardContent>
 			</ButtonBase>
 			<CardActions className={classes.cardActions}>
-				<Button
-					size="small"
-					color="primary"
-					disabled={!user?.result}
-					onClick={() => {
-						dispatch(likePost(post._id));
-					}}
-				>
+				<Button size="small" color="primary" disabled={!user?.result} onClick={handleLike}>
 					<Likes />
 				</Button>
 				{(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
